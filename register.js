c618f2d0aafbf205be0bd9c23d8b6d66d735f58a
Anthony Rojas <anthony.rojas099@gmail.com>
2025-01-06 19:13:22 -0500

    Changes made for Pet Salon

--- register.js
+++ register.js
@@ -30,15 +30,23 @@ function registerPet() {
 
     alert(`${name} has been registered!`);
     document.querySelector("form").reset();
-    displayPets();
+    displayRow();
 }
 
-function displayPets() {
-    const petList = document.getElementById("petList");
-    petList.innerHTML = "";
-    salon.pets.forEach((pet) => {
-        petList.innerHTML += `<li>${pet.name} - ${pet.service}</li>`;
-    });
+function displayRow() {
+    const tableBody = document.getElementById("petTableBody");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${pet.name}</td>
+    <td>${pet.age}</td>
+    <td>${pet.gender}</td>
+    <td>${pet.services}</td>
+    <td><button class="btn btn-danger" onclick=deletePet(this)">Delete</button>`;
+    tableBody.appendChild(row);
+}
+function deletePet(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
 }
 
 document.getElementById("salonInfo").innerHTML = `${salon.name}, located at ${salon.address.street}, Zip: ${salon.address.zip}. Call us: ${salon.phone}`;
@@ -49,4 +57,4 @@ const pet3 = new Pet("Tweety", 70, "Female", "Canary", "Nails", "Bird");
 
 salon.pets.push(pet1, pet2, pet3);
 
-displayPets();
+displayRow();
